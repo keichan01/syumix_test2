af766364173a1506b0067b778787008909d00365
keichan01 <111420389+keichan01@users.noreply.github.com>
2022-08-23 15:17:16 +0900

    Delete config.js

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBX19ljliR1fVS0yVbN3hSfUCkC0KoYAR8",
-    authDomain: "syumix-app.firebaseapp.com",
-    projectId: "syumix-app",
-    storageBucket: "syumix-app.appspot.com",
-    messagingSenderId: "1075527721553",
-    appId: "1:1075527721553:web:e5f1cfcb234ad47fb94e70",
-    measurementId: "G-Y3XSMFT937"
-};
